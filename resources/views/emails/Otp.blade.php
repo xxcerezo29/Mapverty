204cd37d14c228ef079a5f9e839d8de707d018a3
Vincente <vcerezo29@gmail.com>
2023-12-12 23:32:53 +0800

    Fucking

--- resources/views/emails/Otp.blade.php
+++ resources/views/emails/Otp.blade.php
@@ -1,39 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Your OTP</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            line-height: 1.6;
-            color: #333;
-        }
-        .container {
-            max-width: 600px;
-            margin: 0 auto;
-            padding: 20px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            background-color: #f9f9f9;
-        }
-        h1 {
-            color: #007bff;
-        }
-        p {
-            margin-bottom: 15px;
-        }
-        strong {
-            font-weight: bold;
-        }
-    </style>
-</head>
-<body>
-<div class="container">
-    <h1>Your OTP</h1>
-    <p>Dear User,</p>
-    <p>Your OTP is: <strong>{{ $otp }}</strong></p>
-    <p>Please use this OTP for authentication.</p>
-    <p>Thank you!</p>
-</div>
-</body>
-</html>
+@component('mail::message')
+    greetings from {{ config('app.name') }}!
+    {{--    Greetings from Vincent!--}}
+
+    To verify your email address, please use the following One Time Password
+
+    here the Verification Code: <strong>{{ $verification_code }}</strong>
+
+    Do not share this OTP with anyone. MaPverty takes yours information security very seriously.
+    MaPverty will never ask you to disclose or verify your OTP. If you receive a suspicious email
+    with a link to update your personal information, do not click on the link---insteads,
+    report the email to MaPverty for investigation.
+
+    Thank you!
+
+    {{--    Wag ka mabahala, tandaan mo lang ang code na ito: Maganda ka, lalo na cute ka--}}
+
+
+
+    {{--    byessss!!!!--}}
+
+@endcomponent
